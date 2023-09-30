a9299707beac183fa88ba0bb3474288818c41b7f
carlos <carlosvann45@gmail.com>
2023-09-30 18:19:21 -0400

    added types and moved readme

---
+++ packages/skytech-web-components/index.d.ts
@@ -0,0 +1,57 @@
+export declare let SkyTechWebComponents: globalThis;
+export declare let SkyTechProperties: {
+    components: {
+        title: string,
+        name: string,
+        group: string,
+        type: string,
+        properties: [
+            {
+                title: string,
+                name: string,
+                type: string,
+                info?: string,
+                limit?: number,
+                regex?: string,
+                example?: string,
+                values?: [any] 
+            }
+        ]
+    },
+    containers: {
+        title: string,
+        name: string,
+        group: string,
+        type: string,
+        columns: [
+            {
+                title: string,
+                name: string,
+                properties: [
+                    {
+                        title: string,
+                        name: string,
+                        type: string,
+                        info?: string,
+                        limit?: number,
+                        regex?: string,
+                        example?: string,
+                        values?: [any] 
+                    }
+                ]
+            }
+        ],
+        properties: [
+            {
+                title: string,
+                name: string,
+                type: string,
+                info?: string,
+                limit?: number,
+                regex?: string,
+                example?: string,
+                values?: [any] 
+            }
+        ]
+    }
+};
