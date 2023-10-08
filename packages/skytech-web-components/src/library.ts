f077f7aec92e7062f8bbd28d4b97f177be53c0d4
carlos <carlosvann45@gmail.com>
2023-10-08 18:53:35 -0400

    removed console logs for web component library

--- packages/skytech-web-components/src/library.ts
+++ packages/skytech-web-components/src/library.ts
@@ -2,13 +2,13 @@ import fs from 'fs';
 
 function getWebComponentsJs(): string {
     const path = require.resolve('skytech-web-components/index.js');
-    console.log(path);
+    
     return fs.readFileSync(path).toString();
 }
 
 function getWebComponentsCss(): string {
     const path = require.resolve('skytech-web-components/index.css');
-    console.log(path);
+    
     return fs.readFileSync(path).toString();
 }
 
