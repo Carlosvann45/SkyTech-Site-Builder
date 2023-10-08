3215d3a8ae68885d215bcfe376ddea90b2d3e1b1
carlos <carlosvann45@gmail.com>
2023-10-08 15:21:55 -0400

    updated web components package to have correct imports

--- packages/skytech-web-components/vite-library.config.ts
+++ packages/skytech-web-components/vite-library.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: 'custom',
-  plugins: [nodePolyfills()],
+  plugins: [nodePolyfills(), dts()],
   build: {
     outDir: 'dist',
     emptyOutDir: false,
