8e3dd1cbd78cbc95989b5127c0c6f46348cd7f1d
carlos <carlosvann45@gmail.com>
2023-10-07 22:03:01 -0400

    added version update script

--- packages/skytech-web-components/script/moveProperties.js
+++ packages/skytech-web-components/script/moveProperties.js
@@ -1,11 +1,5 @@
 import glob from 'glob';
 import fs from 'fs';
-import packageJson from '../package.json' assert { type: "json" };;
-
-// what part of version to update
-const majorRelease = false;
-const minorRelease = false;
-const patchRelease = true;
 
 // gather all proerties
 const allProperties = {
@@ -32,31 +26,8 @@ for (const file of containerFiles) {
 
 console.log(allProperties);
 
-fs.writeFileSync('./lib/properties.json', JSON.stringify(allProperties, null, 2));
-
-// update version for package.json
-const versionArr = packageJson.version.split('.');
-let majorVersion = parseInt(versionArr[0]);
-let minorVersion = parseInt(versionArr[1]);
-let patchVersion = parseInt(versionArr[2]);
-
-
-if (majorRelease) {
-    majorVersion++;
-}
-
-if (minorRelease) {
-    minorVersion++;
-}
-
-if (patchRelease) {
-    patchVersion++
-}
-
-packageJson.version = `${majorVersion}.${minorVersion}.${patchVersion}`;
-
 // create/move appropriate files for npm release
-fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFileSync('./lib/properties.json', JSON.stringify(allProperties, null, 2));
 fs.copyFileSync('./package.json', './dist/package.json');
 fs.copyFileSync('./index.d.ts', './dist/index.d.ts');
 fs.unlinkSync('./dist/index.html');
---
+++ packages/skytech-web-components/script/updateVersion.js
@@ -0,0 +1,44 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import packageJson from '../package.json' assert { type: "json" };
+
+// check branch name
+exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+    if (err) {
+       console.log(err);
+    }
+
+    if (typeof stdout === 'string' && (stdout.trim() === 'main')) {
+      console.log(`Running Version Update`);
+    } else {
+        throw new Error('You must be on main to publish web components')
+    }
+});
+
+// // what part of version to update
+// const majorRelease = false;
+// const minorRelease = false;
+// const patchRelease = true;
+
+// // update version for package.json
+// const versionArr = packageJson.version.split('.');
+// let majorVersion = parseInt(versionArr[0]);
+// let minorVersion = parseInt(versionArr[1]);
+// let patchVersion = parseInt(versionArr[2]);
+
+
+// if (majorRelease) {
+//     majorVersion++;
+// }
+
+// if (minorRelease) {
+//     minorVersion++;
+// }
+
+// if (patchRelease) {
+//     patchVersion++
+// }
+
+// packageJson.version = `${majorVersion}.${minorVersion}.${patchVersion}`;
+
+// fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
