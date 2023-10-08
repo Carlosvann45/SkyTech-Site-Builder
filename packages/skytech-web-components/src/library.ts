140a52537bb0c0e0308ca22002d91cd9b86381f9
carlos <carlosvann45@gmail.com>
2023-10-08 16:54:45 -0400

    updated web component version for site builder and updated library.ts if web components package

--- packages/skytech-web-components/src/library.ts
+++ packages/skytech-web-components/src/library.ts
@@ -1,11 +1,16 @@
 import fs from 'fs';
+import path from 'path';
+
+const currentDirectory = __dirname;
 
 function getWebComponentsJs(): string {
-    return fs.readFileSync('index.js').toString();
+    console.log(currentDirectory);
+    return fs.readFileSync(path.join(currentDirectory, 'index.js')).toString();
 }
 
 function getWebComponentsCss(): string {
-    return fs.readFileSync('index.css').toString();
+    console.log(currentDirectory);
+    return fs.readFileSync(path.join(currentDirectory, 'index.css')).toString();
 }
 
 export { 
