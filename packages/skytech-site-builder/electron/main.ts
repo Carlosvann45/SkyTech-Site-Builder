814f8a9ed809816f0c98d9768125f64d56052493
carlos <carlosvann45@gmail.com>
2023-11-11 13:20:24 -0500

    updated icon and package.json for installer

--- packages/skytech-site-builder/electron/main.ts
+++ packages/skytech-site-builder/electron/main.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'node:path';
+import url from 'url';
 import fs from 'fs';
 import FileOperations from './utils/FileOperations'
 
@@ -22,10 +23,9 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: __dirname + '/images/favicon.ico',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      devTools: false
+      webSecurity: false
     },
   })
 
@@ -37,8 +37,13 @@ function createWindow() {
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL)
   } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    // win.loadFile(process.env.ELECTRON_START_URL ?? path.join(__dirname, '../index.html'));
+    // win.loadFile(path.join(process.env.DIST, 'index.html'))
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+        }));
   }
 
   win.setMenu(null);
